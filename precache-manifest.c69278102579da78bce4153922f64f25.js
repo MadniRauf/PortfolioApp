358055a4d8fa8b478d66d4bacd8f3e6cfb1fa7b7
Abdul Rauf <madniqaflatdm@gmail.com>
2025-02-04 17:08:52 +0500

    Updates

---
+++ precache-manifest.c69278102579da78bce4153922f64f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f095e296b25e2e58c41b0321c7c58bc1",
+    "url": "/PortfolioApp/index.html"
+  },
+  {
+    "revision": "0dd9e1c94dfc9d843a73",
+    "url": "/PortfolioApp/static/css/main.d78cecb0.chunk.css"
+  },
+  {
+    "revision": "775009af28a9e182f2f0",
+    "url": "/PortfolioApp/static/js/2.9bfbd0ce.chunk.js"
+  },
+  {
+    "revision": "0dd9e1c94dfc9d843a73",
+    "url": "/PortfolioApp/static/js/main.d03eae0e.chunk.js"
+  },
+  {
+    "revision": "1b5033fe105c05ef6ce6",
+    "url": "/PortfolioApp/static/js/runtime~main.09c0c2aa.js"
+  }
+]);
